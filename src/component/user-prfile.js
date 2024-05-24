2cd2ce5943affa2b5175596be9688e869acc5fa6
chinnadurai4395 <92437449+chinnadurai4395@users.noreply.github.com>
2024-05-24 14:22:31 +0530

    initial commit for react js user Profile

---
+++ src/component/user-prfile.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const UserProfile = (props) => {
+
+    console.log(props);
+
+
+    return (
+        <div>
+            <section className="userprfilebacground">
+                <div class="container d-flex justify-content-center align-items-center mb-3">
+                    <div class="card">
+                        <div class="upper">
+                            <img src={props.userCardData.background_image} class="bacImage" alt="background-images" width="100%" height="250px" />
+                        </div>
+
+                        <div class="user text-center">
+                            <div class="profile">
+                                <img src={props.userCardData.avatar} alt="Avator" class="rounded-circle" width="150" />
+                            </div>
+                        </div>
+
+                        <div class="mt-2 text-center profile-body">
+                            <h4 class="mb-0">{props.userCardData.name}</h4>
+                            <span class="text-muted d-block mb-2">{props.userCardData.designation}</span>
+                            <p class="text-muted d-block p-2 text-justify mb-2">{props.userCardData.description}</p>
+                            <button class="btn btn-primary btn-sm follow">Follow</button>
+                            <div class="d-flex justify-content-between align-items-center mt-4 px-4">
+                                <div class="stats">
+                                    <h6 class="mb-0">MAIL</h6>
+                                    <span>{props.userCardData.mail}</span>
+                                </div>
+
+                                <div class="stats">
+                                    <h6 class="mb-0">NATIONALITY</h6>
+                                    <span>{props.userCardData.nationality}</span>
+                                </div>
+
+                                <div class="stats">
+                                    <h6 class="mb-0">PHONE</h6>
+                                    <span>{props.userCardData.phone}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+    )
+}
+
+export default UserProfile;
---
+++ src/component/usercard.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+import user_avatar from '../Assets/user_profile_avatar_png.png';
+import Blue_background from '../Assets/Blue_fog_background.jpg'
+import UserProfile from "./user-prfile";
+
+class UserCard extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            name:'Chinna',
+            date_of_birth: '04-03-1995',
+            gender: 'male',
+            phone: '8887062469',
+            mail:'chinna@gmail.com',
+            background_image:Blue_background,
+            nationality:'Indian',
+            avatar:user_avatar,
+            designation: 'senior software developer',
+            description:'A front-end developer is a professional responsible for designing and implementing the user interface and user experience of a website or web application. Their primary focus is on the visual elements that users interact with, such as layouts, buttons, forms, and navigation menus, as well as ensuring these elements are responsive and accessible across various devices and browsers.'
+        }
+    }
+    
+
+    render(){
+        return <div>
+            <section className="header-section">
+                <h3> User Profile </h3>
+                <UserProfile name='userProfile' userCardData={this.state} />
+            </section>
+        </div>
+    }
+}
+
+
+export default UserCard;
